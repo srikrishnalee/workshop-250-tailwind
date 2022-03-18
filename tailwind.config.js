6975117937013eabecdf3bb957d5e9760b2a037c
srikrishnalee <srikrishna@leewayhertz.com>
2022-03-18 12:25:13 +0530

    adding the workshop files in the git hub

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      padding : {
+        '1/3' : '33.33333%',
+        '2/3' : '66.66667%'
+      },
+    },
+  },
+  plugins: [],
+}
